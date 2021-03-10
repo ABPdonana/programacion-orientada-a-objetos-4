3561f8b3e4b0e2493ef6c3bd98e0f8b69a7143cf
Adrián Bedoya Patino <adrian.bedoya@iesdonana.org>
2021-03-10 15:58:03 +0100

    creado el modulo persona.py

---
+++ persona.py
@@ -0,0 +1,19 @@
+class Persona():
+
+    def _init_(self, nombre, edad):
+        self.__nombre = nombre
+        self.__edad = edad
+
+    def nombre(self):
+        return self.__nombre
+
+    def edad(self):
+        return self.__edad
+
+    def compara_edad(self, otro):
+        if self.edad() > otro.edad():
+            print(f"{otro.nombre()} es más joven que yo.")
+        if self.edad() < otro.edad():
+            print(f"{otro.nombre()} es más viejo que yo.")
+        else
+            print(f"{otro.nombre()} tiene la misma edad que yo.")
